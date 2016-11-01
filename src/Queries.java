b1e59b9021a6145e8dc85a90f3a292d5107c10cb
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-11-01 21:13:48 +0100

    Added queries for indexes

--- src/Queries.java
+++ src/Queries.java
@@ -2,6 +2,7 @@
  * @author Kamil Walkowiak
  */
 class Queries {
+    //Old schema queries
     static String oldSongsCreateQuery = "CREATE TABLE OLD_SONGS " +
             "(ID STRING PRIMARY KEY NOT NULL," +
             "ARTIST STRING," +
@@ -13,17 +14,14 @@ class Queries {
             "TIMESTAMP INTEGER," +
             "FOREIGN KEY (SONG_ID) REFERENCES OLD_SONGS (ID))";
 
+    //New schema queries
     static String songsCreateQuery = "CREATE TABLE SONGS " +
             "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "OLD_ID STRING," +
-            "TITLE STRING);" +
-            "CREATE UNIQUE INDEX SONGS_OLD_ID_IDX " +
-            "ON SONGS (OLD_ID);";
+            "OLD_ID STRING NOT NULL," +
+            "TITLE STRING)";
     static String artistsCreateQuery = "CREATE TABLE ARTISTS " +
             "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "NAME STRING);" +
-            "CREATE UNIQUE INDEX ARTISTS_NAME_IDX " +
-            "ON ARTISTS (NAME);";
+            "NAME STRING NOT NULL)";
     static String playbacksCreateQuery = "CREATE TABLE PLAYBACKS " +
             "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
             "SONG_ID INTEGER NOT NULL," +
@@ -38,20 +36,21 @@ class Queries {
             "FOREIGN KEY (USER_ID) REFERENCES USERS (ID))";
     static String datesCreateQuery = "CREATE TABLE DATES " +
             "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "YEAR INTEGER," +
-            "MONTH INTEGER," +
-            "DAY INTEGER);" +
-            "CREATE UNIQUE INDEX UNIQUE_DATES_IDX " +
-            "ON DATES (YEAR, MONTH, DAY);";
+            "YEAR INTEGER NOT NULL," +
+            "MONTH INTEGER NOT NULL," +
+            "DAY INTEGER NOT NULL)";
     static String timesCreateQuery = "CREATE TABLE TIMES " +
             "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "HOUR INTEGER," +
-            "MINUTE INTEGER);" +
-            "CREATE UNIQUE INDEX UNIQUE_TIMES_IDX " +
-            "ON TIMES (HOUR, MINUTE);";
+            "HOUR INTEGER NOT NULL," +
+            "MINUTE INTEGER NOT NULL)";
     static String usersCreateQuery = "CREATE TABLE USERS " +
             "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "OLD_ID STRING);" +
-            "CREATE UNIQUE INDEX USERS_OLD_ID_IDX " +
-            "ON USERS (OLD_ID);";
+            "OLD_ID STRING NOT NULL)";
+
+    //New schema indexes queries
+    static String indexOnSongIdFkCreateQuery = "CREATE INDEX SONG_ID_FK ON PLAYBACKS (SONG_ID)";
+    static String indexOnArtistIdFkCreateQuery = "CREATE INDEX ARTIST_ID_FK ON PLAYBACKS (ARTIST_ID)";
+    static String indexOnDateIdFkCreateQuery = "CREATE INDEX DATE_ID_FK ON PLAYBACKS (DATE_ID)";
+    static String indexOnTimeIdFkCreateQuery = "CREATE INDEX TIME_ID_FK ON PLAYBACKS (TIME_ID)";
+    static String indexOnUserIdFkCreateQuery = "CREATE INDEX USER_ID_FK ON PLAYBACKS (USER_ID)";
 }
